c9da6e089d1aa5a5e160b5c0a76fd2dde210bcb9
Kirill Valikov <Kirill.Valikov@infowatch.com>
2024-12-10 12:11:01 +0300

    add metrics + fix text

--- src/AfterInstall.jsx
+++ src/AfterInstall.jsx
@@ -4,7 +4,7 @@ function AfterInstall() {
   return (
     <>
       <h2 className="header">Edit Website Extension installed!</h2>
-      <div className='descr'>Click the pencil icon (1) to start editing</div>
+      <div className='descr'>Click the pencil icon (1) located on the right side of your browser window to begin editing</div>
       <div className='descr'>Please try editing this sample text (2)</div>
       <div className='descr'>Click the pencil icon (1) to stop editing</div>
       <img className='img' src="./after_install.png" alt="" />
