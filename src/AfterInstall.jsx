d9a634ca564371a62b16d6c81485023ce6062d7f
Kirill Valikov <Kirill.Valikov@infowatch.com>
2024-12-22 21:11:16 +0300

    fix

--- src/AfterInstall.jsx
+++ src/AfterInstall.jsx
@@ -6,6 +6,7 @@ function AfterInstall() {
       <h2 className="header">Google Meet Create installed!</h2>
       <div className='descr'>Click the puzzle piece (1) in the top right of your browser.</div>
       <div className='descr'>Then, click the little pin (2) next to the extension:</div>
+      <img className='img' src="./pin.png" alt="" />
       <div className='descr'>Activate the extension (3):</div>
       <img className='img' src="./run.png" alt="" />
     </>
